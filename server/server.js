547cabb52a85e2dcbbe99890b59d7c8d5dde4993
knutole <knutole@mapic.io>
2016-11-09 20:02:26 +0100

    callback when mongo is ready

--- server/server.js
+++ server/server.js
@@ -25,89 +25,86 @@ var port = config.port;
 var app = express().http().io();
 
 // connect to our database
-// var sessionStore = mongoose.connect(config.mongo.url); 
-var mongoSession = connect_to_mongo();
+connect_to_mongo(function (err) {
 
-// set up our express application
-app.use(bodyParser.urlencoded({limit: '2000mb', extended : true}));
-app.use(bodyParser.json({limit:'2000mb'}));
-app.set('view engine', 'ejs'); // set up ejs for templating
-app.use(multipart()); // for resumable.js uploads
+	// set up our express application
+	app.use(bodyParser.urlencoded({limit: '2000mb', extended : true}));
+	app.use(bodyParser.json({limit:'2000mb'}));
+	app.set('view engine', 'ejs'); // set up ejs for templating
+	app.use(multipart()); // for resumable.js uploads
 
-// cookie session options
-var sessionOptions = {
-	cookieName: 'session',
-	secret: 'eg[isfd-8yF9-7w2335df{}+Ijsli;;to8', 
-	duration: 3 * 24 * 60 * 60 * 1000, // 3d
-	activeDuration: 60 * 60 * 10000, // 10h
-	cookie: {
-		path: '/', 
-		ephemeral: false, // when true, cookie expires when the browser closes
-		httpOnly: true, // when true, cookie is not accessible from javascript
-		secureProxy : true,
-	}
-};
-
-// use cookie session
-app.use(clientSession(sessionOptions));
-
-// socket auth middleware
-app.io.use(socket_auth_middleware);
-
-// set favicon
-app.use(favicon(__dirname + '/../public/images/favicon.ico'));
-
-// enable compression
-app.use(compress());
-
-// enable cors
-app.use(cors());
-
-// static files
-var staticPath = '../public';
-app.use(express.static(path.join(__dirname, staticPath)));
-app.use(express.static(process.cwd() + '../views/debug'));
-
-// catch route errors
-app.use(function(err, req, res, next){ 
-	err.status === 400 ? res.render('../../views/index.ejs') : next(err);
-});
-
-// load our routes and pass in our app
-require('../routes/routes.js')(app);
-
-// load our socket api
-require('../routes/socket.routes.js')(app);
-
-// when mongoose is ready
-mongoSession.connection.on('open', function (ref) {
-
-	// launch 
-	var server = app.listen(port);
-	console.log('The magic happens @ ', port);
-
-  	// create admin user
-	api.user.ensureAdminUser(function (err, options) {
-		if (err) return console.log('There was an error creating mapic-admin user.', err);
-		if (options.created) {
-			console.log('User mapic-admin has been created. Log in to the portal with these credentials:')
-			console.log('Email:', options.user.local.email);
-			console.log('Password:', options.password);
+	// cookie session options
+	var sessionOptions = {
+		cookieName: 'session',
+		secret: 'eg[isfd-8yF9-7w2335df{}+Ijsli;;to8', 
+		duration: 3 * 24 * 60 * 60 * 1000, // 3d
+		activeDuration: 60 * 60 * 10000, // 10h
+		cookie: {
+			path: '/', 
+			ephemeral: false, // when true, cookie expires when the browser closes
+			httpOnly: true, // when true, cookie is not accessible from javascript
+			secureProxy : true,
 		}
+	};
+
+	// use cookie session
+	app.use(clientSession(sessionOptions));
+
+	// socket auth middleware
+	app.io.use(socket_auth_middleware);
+
+	// set favicon
+	app.use(favicon(__dirname + '/../public/images/favicon.ico'));
+
+	// enable compression
+	app.use(compress());
+
+	// enable cors
+	app.use(cors());
+
+	// static files
+	var staticPath = '../public';
+	app.use(express.static(path.join(__dirname, staticPath)));
+	app.use(express.static(process.cwd() + '../views/debug'));
+
+	// catch route errors
+	app.use(function(err, req, res, next){ 
+		err.status === 400 ? res.render('../../views/index.ejs') : next(err);
 	});
+
+	// load our routes and pass in our app
+	require('../routes/routes.js')(app);
+
+	// load our socket api
+	require('../routes/socket.routes.js')(app);
+
+	// when mongoose is ready
+	mongoose.connection.on('open', function (ref) {
+
+		// launch 
+		var server = app.listen(port);
+		console.log('The magic happens @ ', port);
+
+	  	// create admin user
+		api.user.ensureAdminUser(function (err, options) {
+			if (err) return console.log('There was an error creating mapic-admin user.', err);
+			if (options.created) {
+				console.log('User mapic-admin has been created. Log in to the portal with these credentials:')
+				console.log('Email:', options.user.local.email);
+				console.log('Password:', options.password);
+			}
+		});
+	});
+
 });
 
-function connect_to_mongo () {
-	console.log('Attempting to connect to mongo.');
-
-	try {
-		var mongoSession = mongoose.connect(config.mongo.url);
-		return mongoSession; 
-	} catch (e) {
-		sleep.sleep(1);
-		return connect_to_mongo();
-	}
-
+// helper fn
+function connect_to_mongo (done) {
+	mongoose.connect(config.mongo.url, function (err) {
+		if (!err) return done();
+		sleep.sleep(2);
+		return connect_to_mongo(done);
+	});
 }
 
 // helper fn
@@ -135,12 +132,3 @@ function socket_auth_middleware (socket, next) {
 		next(new Error('Something went wrong.'));
 	}
 }
-
-process.on('uncaughtException', function(err) {
-	// todo: add sentry.io
-    console.log('Uncaught Exception!', err);
-    throw new Error(err);
-});
-
-
-

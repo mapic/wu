bf5f743b7bc83e2d1cf89472ea5db65cf5734239
UbuntuMain14.04.2 <shahjada.talukdar@gmail.com>
2016-12-13 15:46:18 -0500

    projects/slug/unique Test Fixed

--- test/projects/unique.js
+++ test/projects/unique.js
@@ -23,12 +23,12 @@ module.exports = function () {
         it('should respond with status code 200', function (done) {
             token(function (err, access_token) {
                 api.post(endpoints.projects.slug.unique)
-                    .send({access_token: access_token}) // ?? should send slug?
+                    .send({access_token: access_token , slug : "its-a-slug" , createdByClient : 'admin' , uuid : '' }) // ?? should send slug?
                     .expect(httpStatus.OK)
                     .end(function (err, res) {
                         if (err) return done(err);
                         var result = helpers.parse(res.text);
-                        expect(result.unique).to.be.true;
+                        expect(result.uniqueSlug).to.be.true;
                         done();
                     });
             });

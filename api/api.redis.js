33c01153efebb438b8ec9f63361843763214981e
knutole <knutole@mapic.io>
2017-07-25 16:27:36 +0000

    redis: automatic retry of authentication

--- api/api.redis.js
+++ api/api.redis.js
@@ -3,23 +3,54 @@ var api = module.parent.exports;
 
 // config
 var config = require('../config.js').serverConfig;
-// todo: config must be defined like this, because of redis is used in test/helpers.js
-// this is kinda fubar. fix!
 
-// redis store for layers
+// redis stores
 var redisLayers = require('redis').createClient(config.redis.layers.port, config.redis.layers.host);
 redisLayers.on('error', function (err) {console.log('Redis error: ', err);});
-redisLayers.auth(config.redis.layers.auth);
-
-// redis store for stats
 var redisStats = require('redis').createClient(config.redis.stats.port, config.redis.stats.host);
 redisStats.on('error', function (err) {console.log('Redis error: ', err);});
-redisStats.auth(config.redis.stats.auth);
-
-// redis store for temp tokens and upload increments
 var redisTemp = require('redis').createClient(config.redis.temp.port, config.redis.temp.host);
 redisTemp.on('error', function (err) {console.log('Redis error: ', err);});
-redisTemp.auth(config.redis.temp.auth);
+
+function redisLayersAuth () {
+    redisLayers.auth(config.redis.layers.auth, function (err) {
+        if (err) {
+            console.log('redisLayers auth error: ', err);
+            console.log('Retrying...')
+            setTimeout(redisLayersAuth, 500);
+        } else {    
+            console.log('Redis Layers authenticated OK')
+        }
+    });
+}
+function redisStatsAuth () {
+    redisStats.auth(config.redis.stats.auth, function (err) {
+        if (err) {
+            console.log('redisStats auth error: ', err);
+            console.log('Retrying...')
+            setTimeout(redisStatsAuth, 500);
+        } else {    
+            console.log('Redis Stats authenticated OK')
+        }
+    });
+}
+function redisTempAuth () {
+    redisTemp.auth(config.redis.temp.auth, function (err) {
+        if (err) {
+            console.log('redisTemp auth error: ', err);
+            console.log('Retrying...')
+            setTimeout(redisTempAuth, 500);
+        } else {    
+            console.log('Redis Temp authenticated OK')
+        }
+    });
+}
+
+
+// auth with automatic retry
+redisLayersAuth();
+redisStatsAuth();
+redisTempAuth();
 
 // exports
 module.exports = api.redis = { 

76426be6522fa87d0e1e34691a495700dba8253e
knutole <knutole@mapic.io>
2017-10-12 12:18:32 +0000

    api: list/delete project

--- scripts/delete_project.js
+++ scripts/delete_project.js
@@ -1,16 +1,16 @@
 // libs
 var async 	 = require('async');
 var colors 	 = require('colors');
-var crypto       = require('crypto');
+var crypto   = require('crypto');
 var uuid 	 = require('node-uuid');
-var mongoose 	 = require('mongoose');
+var mongoose = require('mongoose');
 var _ 		 = require('lodash');
 var fs 		 = require('fs');
 var prompt 	 = require('prompt');
 
 // database schemas
-var Project 	 = require('../models/project');
-var Clientel 	 = require('../models/client');	// weird name cause 'Client' is restricted name
+var Project  = require('../models/project');
+var Clientel = require('../models/client');	// weird name cause 'Client' is restricted name
 var User  	 = require('../models/user');
 var File 	 = require('../models/file');
 var Layer 	 = require('../models/layer');
@@ -24,15 +24,12 @@ var config  = require('../config.js').serverConfig;
 // connect to our database
 mongoose.connect(config.mongo.url); 
 
-
-
 var projectUuid = process.argv[2];
 
 if (!projectUuid) {
 	console.log('Usage: node delete_project.js projectUuid');
 	return process.exit(0);
 }
-
 
 Project
 .findOne({uuid : projectUuid})
@@ -43,39 +40,25 @@ Project
 		return;
 	}
 
-
 	console.log('Project to delete:'.red, project.name);
 
-	prompt.get({
-		properties : {
-			confirm : {
-				description : 'Does this look right? Write [yes] to go ahead and delete user'.yellow
-			}
-		}
-	}, function (err, answer) {
-		if (err || answer.confirm != 'yes') {
-			console.log('Aborting!'.red);
-			return process.exit(0);
-		}
-		
-		Project
-		.findOne({uuid : projectUuid})
-		.remove(function (err, project) {
+	Project
+	.findOne({uuid : projectUuid})
+	.remove(function (err, project) {
 
-			if (err || !project) {
-				console.log('Something went wrong. Exiting.');
-				process.exit(0);
-				return;
-			}
-
-			var result = project.result;
-
-			if (result.ok) {
-				console.log('Deleted!');
-			}
-
+		if (err || !project) {
+			console.log('Something went wrong. Exiting.');
 			process.exit(0);
-		})
-	});
+			return;
+		}
+
+		var result = project.result;
+
+		if (result.ok) {
+			console.log('Deleted!');
+		}
+
+		process.exit(0);
+	})
 	
 });

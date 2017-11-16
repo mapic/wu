77471dded3fc06233c5d230197193cd3ac2eb79f
knutole <knutole@mapic.io>
2017-11-16 08:29:24 +0000

    bugfix

--- scripts/postgis/enter_db.sh
+++ scripts/postgis/enter_db.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-. `dirname $0`/run_in_docker.inc
+# . `dirname $0`/run_in_docker.inc
 
 export PAGER="/usr/bin/less -S"
 export PGPASSWORD=$MAPIC_POSTGIS_PASSWORD
 export PGUSER=$MAPIC_POSTGIS_USERNAME
 export PGHOST=$MAPIC_POSTGIS_HOST
-export PGDATABSE=template1
+export PGDATABASE=template1
 
 psql -h postgis $@

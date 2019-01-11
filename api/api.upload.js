7272c109f0d9d87af26d61a101c8fcb7f027bffa
knutole <knutole@mapic.io>
2019-01-11 15:19:21 +0000

    fix missing status error

--- api/api.upload.js
+++ api/api.upload.js
@@ -447,8 +447,10 @@ module.exports = api.upload = {
 	},
 
 	_setStatusError : function (status, err) {
-	
+		console.log('status error:', err);
+		
 		// set error to upload status
+		status = status || {};
 		status.upload_success = false;
 		status.status = 'Failed';
 		status.error = true;
